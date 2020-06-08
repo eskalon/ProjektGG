c747efd71d4fdc711204f4c88fb109c0115d3374
crykn <crykn@users.noreply.github.com>
2020-06-08 22:22:46 +0200

    DesktopLauncher is now using a StartOnFirstThreadHelper.

--- desktop/src/main/java/de/gg/desktop/DesktopLauncher.java
+++ desktop/src/main/java/de/gg/desktop/DesktopLauncher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+import de.damios.guacamole.gdx.StartOnFirstThreadHelper;
 import de.damios.guacamole.gdx.Sync;
 import de.gg.game.core.ProjektGGApplication;
 
@@ -14,32 +15,35 @@ import de.gg.game.core.ProjektGGApplication;
 public class DesktopLauncher {
 
 	public static void main(String[] args) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle(ProjektGGApplication.NAME);
-		config.setWindowedMode(1280, 720);
-		config.setResizable(false);
-		config.useVsync(false);
-		config.setWindowIcon(FileType.Internal, "icon16.png", "icon32.png",
-				"icon48.png");
+		StartOnFirstThreadHelper.executeIfJVMValid(() -> {
+			Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+			config.setTitle(ProjektGGApplication.NAME);
+			config.setWindowedMode(1280, 720);
+			config.setResizable(false);
+			config.useVsync(false);
+			config.setWindowIcon(FileType.Internal, "icon16.png", "icon32.png",
+					"icon48.png");
 
-		try {
-			new Lwjgl3Application(new ProjektGGApplication() {
-				private Sync sync = new Sync();
+			try {
+				new Lwjgl3Application(new ProjektGGApplication() {
+					private Sync sync = new Sync();
 
-				@Override
-				public void render() {
-					super.render();
+					@Override
+					public void render() {
+						super.render();
 
-					if (!Gdx.graphics.isFullscreen())
-						sync.sync(Gdx.graphics.getDisplayMode().refreshRate);
-				}
-			}, config);
-		} catch (Exception e) {
-			System.err.println(
-					"An unexpected error occurred while starting the game:");
-			e.printStackTrace();
-			System.exit(-1);
-		}
+						if (!Gdx.graphics.isFullscreen())
+							sync.sync(
+									Gdx.graphics.getDisplayMode().refreshRate);
+					}
+				}, config);
+			} catch (Exception e) {
+				System.err.println(
+						"An unexpected error occurred while starting the game:");
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		});
 	}
 
 }
